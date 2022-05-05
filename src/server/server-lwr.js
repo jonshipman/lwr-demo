258dd1c1cb52942c200c7febc9c26690f8310ac3
Jon Shipman <jon@jonshipman.com>
2022-05-05 14:39:13 -0500

    Nested Routes do not work under prod (serverMode needs to be prod-compat)

--- src/server/server-lwr.js
+++ src/server/server-lwr.js
@@ -3,7 +3,7 @@ import { createServer } from 'lwr';
 const LWR_PORT = parseInt(process.env.LWR_PORT || '3001');
 
 const lwrServer = createServer({
-	serverMode: 'development' === process.env.NODE_ENV ? 'dev' : 'prod',
+	serverMode: 'development' === process.env.NODE_ENV ? 'dev' : 'prod-compat',
 	port: LWR_PORT,
 });
 
